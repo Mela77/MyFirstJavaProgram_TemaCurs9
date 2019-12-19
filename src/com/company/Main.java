75953b847611638af43eed43fd80f6223bbffcf4
Mella77 <mela.pandrea@yahoo.com>
2019-12-19 20:37:00 +0200

    Hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+
+        int x=4;
+        int y=3;
+        float sum = x + y;
+
+        System.out.println(sum);
+
+        String Today = "Today's Thursday";
+        System.out.println("today");
+
+    }
+}
